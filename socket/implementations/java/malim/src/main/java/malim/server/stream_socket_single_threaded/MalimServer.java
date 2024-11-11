a3a2c80233879b5170bd379a7d4a7ab434678623
Seyedmasih Tabaei <masihtabaei@gmail.com>
2024-11-11 20:50:42 +0100

    Made a contribution to #6
    Improved the error-handling of createServerSocket-method

--- socket/implementations/java/malim/src/main/java/malim/server/stream_socket_single_threaded/MalimServer.java
+++ socket/implementations/java/malim/src/main/java/malim/server/stream_socket_single_threaded/MalimServer.java
@@ -9,28 +9,32 @@ public class MalimServer {
     private String serverName;
     private ServerSocket serverSocket;
 
-    public MalimServer() throws IOException{
+    public MalimServer(){
         this(0);
     }
 
-    public MalimServer(int portNumber) throws IOException{
+    public MalimServer(int portNumber){
         this(portNumber, 1);
     }
 
-    public MalimServer(int portNumber, int connectionQueueMaximumSize) throws IOException{
+    public MalimServer(int portNumber, int connectionQueueMaximumSize){
         this(portNumber, connectionQueueMaximumSize, "Malim Server Default Name");
     }
 
-    public MalimServer(int portNumber, int connectionQueueMaximumSize, String serverName) throws IOException{
+    public MalimServer(int portNumber, int connectionQueueMaximumSize, String serverName){
         this.portNumber = portNumber;
         this.connectionQueueMaximumSize = connectionQueueMaximumSize;
         this.serverName = serverName;
         createServerSocket();
     }
 
-    private void createServerSocket() throws IOException{
+    private void createServerSocket(){
         if(serverSocket == null){
-            serverSocket = new ServerSocket(portNumber, connectionQueueMaximumSize);
+            try {
+                serverSocket = new ServerSocket(portNumber, connectionQueueMaximumSize);
+            } catch (IOException exceptionCaught) {
+                System.err.println(exceptionCaught.getMessage());
+            }
         }
     }
 
